64bf0b65435bb5ff4bba49df87572617560ecdcd
Justin <33402995+logJustin@users.noreply.github.com>
2025-05-14 23:48:09 -0500

    cursor

--- src/app/burgers/page.tsx
+++ src/app/burgers/page.tsx
@@ -1,11 +1,13 @@
 import AddBurgerModal from "@/components/custom/burgers/AddBurgersModal";
 import BurgerGrid from "@/components/custom/burgers/BurgerGrid";
+import Spinner from "@/components/custom/Spinner";
 import { createClient } from "@/utils/supabase/server";
 
 export default async function Page() {
   const supabase = await createClient();
-  const { data: burgers } = await supabase.from("test").select();
+  const { data: burgers } = await supabase.from("burgers").select();
 
+  if (!burgers) return <Spinner />;
   return (
     <div className="flex justify-center px-4 md:px-8 lg:px-0 w-full min-h-screen">
       <div className="w-full max-w-4xl flex flex-col gap-y-6">
@@ -13,7 +15,7 @@ export default async function Page() {
           <Header />
           <AddBurgerModal />
         </div>
-        <BurgerGrid burgers={burgers || []} />
+        {burgers?.length > 0 ? <BurgerGrid burgers={burgers || []} /> : <BurgerPlacholder />}
       </div>
     </div>
   );
@@ -26,4 +28,8 @@ function Header() {
       <div className="text-xl sm:text-3xl md:text-4xl font-bold text-white tracking-tight">Burger Boi’s Finest</div>
     </div>
   );
+}
+
+function BurgerPlacholder() {
+  return <div className="p-4 text-3xl text-center bg-primary/10 rounded-md shadow-md">No Burgers</div>;
 }
